d96bfeb67aa50d736f1d509bf9ebdbf766709569
Cristhoper Mendez <51303481+Cristhoper-Mendez@users.noreply.github.com>
2021-08-17 17:17:54 -0600

    Formateando el código

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home";
-import Favourites from "./components/Favourites";
+import Favourites from "./components/Favourites/Favourites";
 import CharacterState from "./Context/CharacterState";
 
 function App() {
   return (
-      <CharacterState>
-    <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/favourites" component={Favourites} />
-        </Switch>
-    </Router>
-      </CharacterState>
+    <CharacterState>
+      <div className="Contenedor-app">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/favourites" component={Favourites} />
+          </Switch>
+        </Router>
+      </div>
+    </CharacterState>
   );
 }
 
